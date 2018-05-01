94302e59554929a78b1bcd910fc8edc5954e8210
NooTriX-Technology <38850649+NooTriX-Technology@users.noreply.github.com>
2018-05-01 11:36:51 +0200

    Added check servo voltage sample sketch

---
+++ Sketches/CheckVoltage/CheckVoltage.ino
@@ -0,0 +1,54 @@
+/*    Copyright 2018 NooTriX (https://nootrix.com)
+ *  
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ */
+#include <ax12.h> //ax12 library allows to send DYNAMIXEL commands
+
+void setup(){
+  Serial.begin(9600);
+  delay (500);   
+  Serial.println("Serial Communication Established.");
+}
+
+unsigned long lastDisplayTime = 0;
+unsigned long millisecondsBetweenDisplayRefresh = 2000;
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  unsigned long currentTime = millis();
+  if(currentTime - lastDisplayTime < millisecondsBetweenDisplayRefresh){
+    return;
+  }
+  lastDisplayTime = currentTime; 
+  checkVoltage();
+}
+
+const int NumberOfServos = 4;
+const int ServoIDs[] = {1, 2, 3, 4};
+
+void checkVoltage(){
+  //ServoIDs is optional if servos IDs are consecutive starting at 1
+  float voltage = dxlGetSystemVoltage(NumberOfServos, ServoIDs); 
+  Serial.print ("##### System Voltage: ");
+  Serial.print (voltage);
+  Serial.println (" volts.");
+  if (voltage >= 10.0){
+    Serial.println("ALL GOOD: Voltage is above 10 V");
+    return;
+  }
+  Serial.println("PROBLEM: Voltage level below minimum (10 V)");
+  Serial.println("DANGER if you are using LiPO batteries!!!");
+}
+
